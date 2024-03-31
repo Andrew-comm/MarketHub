38b99321776c395a29bd4059cb9e589c77a19315
andrea <ndrwrono2001@gmail.com>
2024-04-01 00:33:54 +0300

    updated system and enabled filters and price filtering

--- shopFusion/MarketHub/views.py
+++ shopFusion/MarketHub/views.py
@@ -4,6 +4,8 @@ from .models import  Category, Product, SubCategory
 from django.contrib.auth import login, authenticate, logout
 from .forms import UserRegistrationForm
 from django.contrib.auth.models import User
+from django.db.models import Q
+
 
 
 
@@ -63,18 +65,54 @@ def logout_view(request):
 def product_list_by_category(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
     subcategories = SubCategory.objects.filter(category=category)
+    
+    # Retrieve search query from GET parameters
+    query = request.GET.get('q')
+    
+    # Retrieve minimum and maximum price values from GET parameters
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    
+    # Filter products by category and subcategory
     products_by_subcategory = []
     for subcategory in subcategories:
         products = Product.objects.filter(category=category, subcategory=subcategory)
+        
+        # If query exists, filter products by name or description
+        if query:
+            products = products.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+            )
+        
+        # Filter products by price range
+        if min_price is not None:
+            products = products.filter(price__gte=min_price)
+        if max_price is not None:
+            products = products.filter(price__lte=max_price)
+        
         products_by_subcategory.append({'subcategory': subcategory, 'products': products})
-    return render(request, 'product_list.html', {'category': category, 'products_by_subcategory': products_by_subcategory})
+    
+    # Pass the search results to the template if a query exists
+    if query:
+        search_results = [product for subcategory_data in products_by_subcategory for product in subcategory_data['products']]
+        return render(request, 'product_list.html', {'category': category, 'search_results': search_results, 'min_price': min_price, 'max_price': max_price})
+    
+    return render(request, 'product_list.html', {'category': category, 'products_by_subcategory': products_by_subcategory, 'min_price': min_price, 'max_price': max_price})
 
 def product_list_by_subcategory(request, category_id, subcategory_id):
     category = get_object_or_404(Category, pk=category_id)
     subcategory = get_object_or_404(SubCategory, pk=subcategory_id)
-    products = Product.objects.filter(category=category, subcategory=subcategory)
-    return render(request, 'product_list.html', {'category': category, 'subcategory': subcategory, 'products': products})
+    
+    # Get the search query from the request parameters
+    query = request.GET.get('q')
 
+    # Filter products based on category, subcategory, and search query
+    products = Product.objects.filter(category=category, subcategory=subcategory)
+    
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'product_list.html', {'category': category, 'subcategory': subcategory, 'products': products})
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
@@ -85,7 +123,7 @@ def display_two_products(request):
     categories = Category.objects.all()
     category_products = []
     for category in categories:
-        products = Product.objects.filter(category=category)[:3]  # Retrieve only two products per category
+        products = Product.objects.filter(category=category)[:4]  # Retrieve some products per category
         category_products.append({'category': category, 'products': products})
     return render(request, 'display_two_products.html', {'category_products': category_products})
 
--- shopFusion/shopFusion/settings.py
+++ shopFusion/shopFusion/settings.py
@@ -137,3 +137,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'products'
